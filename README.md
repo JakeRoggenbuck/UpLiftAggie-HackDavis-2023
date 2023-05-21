10cc9c2442051f27c7f316ef882f5009a6da39d6
Jake <jakeroggenbuck2@gmail.com>
2023-05-21 10:07:04 -0700

    Backend almost all the way done!

---
+++ README.md
@@ -0,0 +1,4 @@
+# UpLiftAggie
+
+![Image](./upliftaggie.jpeg)
+
--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+# will be replaced later
+DATABASE = []
+
+
+class Message(BaseModel):
+    text: str
+    views: int | None = None
+
 
 app = FastAPI()
 
+# use open api to determine this
+def check_is_good_message(msg: str):
+    return False
 
-@app.get("/")
-def read_root():
+
+@app.get("/get_message")
+def get_message():
     return {"Hack": "Davis"}
+
+
+@app.post("/send_message")
+def send_message(msg: Message):
+    msg.views = 0
+
+    if check_is_good_message(msg.text):
+        DATABASE.append(msg)
+
+    return "Thanks!"
