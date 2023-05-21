402afaba106b864fb7fe64965547c8a62039ab7a
Jake <jakeroggenbuck2@gmail.com>
2023-05-21 10:37:06 -0700

    Add message checking

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import openai
 import random
+from os import getenv
+
+openai.organization = getenv("OPENAI_API_ORG")
+openai.api_key = getenv("OPENAI_API_KEY")
 
 
 class Message(BaseModel):
@@ -18,7 +23,22 @@ app = FastAPI()
 
 # use open api to determine this
 def check_is_good_message(msg: str):
-    return True
+    # other classification systems work but this is an experiment
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "user",
+                "content": f"respond with only YES if the following message is\
+                    exclusively positive and nice and is relevant to being kind\
+                    to another person: {msg}",
+            },
+        ],
+    )
+
+    is_yes = completion["choices"][0]["message"]["content"]
+    print(is_yes)
+    return is_yes == "YES" or is_yes == "YES."
 
 
 @app.get("/get_message")
@@ -39,5 +59,6 @@ def send_message(msg: Message):
 
     if check_is_good_message(msg.text):
         DATABASE.append(msg)
+        return "Thanks!"
 
-    return "Thanks!"
+    return "Sent"
